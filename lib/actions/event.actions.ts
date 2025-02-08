24c8465acb098ae582238825d06dbf3da7ceef29
John Doe <johndoe@email.com>
2025-02-08 03:04:18 +0000

    created EventDetail page; actions for get event by id, get all events, delete event; created Card, Collection and Delete components

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -17,6 +17,16 @@ import {
   GetRelatedEventsByCategoryParams,
 } from "@/types";
 
+const populateEvent = (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
+};
+
 // CREATE
 export async function createEvent({ userId, event, path }: CreateEventParams) {
   try {
@@ -33,6 +43,70 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
     revalidatePath(path);
 
     return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// GET EVENT BY ID
+export async function getEventById(eventId: string) {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) throw new Error("Event not found");
+
+    return JSON.parse(JSON.stringify(event));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// DELETE
+export async function deleteEvent({ eventId, path }: DeleteEventParams) {
+  try {
+    await connectToDatabase()
+
+    const deletedEvent = await Event.findByIdAndDelete(eventId)
+    if (deletedEvent) revalidatePath(path)
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+
+// GET ALL EVENTS
+export async function getAllEvents({
+  query,
+  limit = 6,
+  page,
+  category,
+}: GetAllEventsParams) {
+  try {
+    await connectToDatabase();
+
+    const titleCondition = query
+      ? { title: { $regex: query, $options: "i" } }
+      : {};
+    // const categoryCondition = category ? await getCategoryByName(category) : null
+    const conditions = {
+      // $and: [titleCondition, categoryCondition ? { category: categoryCondition._id } : {}],
+    };
+
+    const skipAmount = (Number(page) - 1) * limit;
+    const eventsQuery = Event.find(conditions)
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const events = await populateEvent(eventsQuery);
+    const eventsCount = await Event.countDocuments(conditions);
+
+    return {
+      data: JSON.parse(JSON.stringify(events)),
+      totalPages: Math.ceil(eventsCount / limit),
+    };
   } catch (error) {
     handleError(error);
   }
